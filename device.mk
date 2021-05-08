c50800f1866cf022779e1cc3e387723e535a5a3f
bgcngm <bgcngm@users.noreply.github.com>
2021-05-08 20:10:50 +0000

    RMX1921: Move to new display HAL
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -125,9 +125,26 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     gralloc.sdm710 \
     memtrack.sdm710 \
+    hwcomposer.sdm710 \
     libtinyxml \
     libvulkan \
-    vendor.display.config@1.7 \
+    libdisplayconfig \
+    libdisplayconfig.vendor \
+    libdisplayconfig.qti \
+    libqdMetaData \
+    libqdMetaData.system \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
+    vendor.display.config@1.2.vendor \
+    vendor.display.config@1.3.vendor \
+    vendor.display.config@1.4.vendor \
+    vendor.display.config@1.5.vendor \
+    vendor.display.config@1.6.vendor \
+    vendor.display.config@1.7.vendor \
+    vendor.display.config@1.8.vendor \
+    vendor.display.config@1.9.vendor \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator@1.0-service
 
 # DRM
@@ -217,6 +234,9 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    libminijail \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
     libc2dcolorconvert \
     libOmxAacEnc \
     libOmxAmrEnc \
@@ -467,12 +487,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libdisplayconfig \
-    libdisplayconfig.vendor \
-    libnl \
-    libqdMetaData \
-    libqdMetaData.system \
-    libwfdaac_proprietary
+    libnl
 
 # PRODUCT_BOOT_JARS += \
     WfdCommon
