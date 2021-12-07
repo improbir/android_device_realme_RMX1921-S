174c9adf33d3fc0cd67b9ad412869faf2cdcac40
improbir <ghosh_probir@yahoo.com>
2021-12-07 06:41:14 +0000

    RMX1921: Drop RealmeParts for now
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -482,7 +482,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # RealmeParts
-PRODUCT_PACKAGES += \
-    RealmeParts
+#PRODUCT_PACKAGES += \
+#    RealmeParts
 
   
