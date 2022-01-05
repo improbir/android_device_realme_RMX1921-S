acae5c872af79d76b01830e68c3de2ca2dd97723
improbir <ghosh_probir@yahoo.com>
2022-01-05 12:04:16 +0000

    RMX1921: build RealmeDirac
    * Also need to clone this repo in your rom source:- https://github.com/improbir/packages_apps_RealmeDirac.git
    
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@ $(call inherit-product-if-exists, vendor/realme/RMX1921/RMX1921-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# RealmeDirac
+$(call inherit-product, packages/apps/RealmeDirac/dirac.mk)
+
 # HIDL
 $(call inherit-product, $(LOCAL_PATH)/RMX1921-hidl.mk)
 
