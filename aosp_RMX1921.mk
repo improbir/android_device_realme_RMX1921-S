55c8c670a0fdf05778c3b8cf075986c03d58b9c8
improbir <ghosh_probir@yahoo.com>
2022-02-10 01:37:55 +0000

    RMX1921: Switch to raven february build fp
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- aosp_RMX1921.mk
+++ aosp_RMX1921.mk
@@ -23,10 +23,10 @@ PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys" \
+        PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys" \
         PRODUCT_NAME="RMX1921" \
 	TARGET_DEVICE="RMX1921"
 
