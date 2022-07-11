285d49ecaaf02e947022d435c0852bc6cfecca72
improbir <ghosh_probir@yahoo.com>
2022-07-11 14:14:36 +0000

    RMX1921: Add vendorsetup
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+#Kernel Tree
+git clone https://github.com/improbir/kernel_realme_sdm710.git -b snow kernel/realme/sdm710 
+
+#Vendor Tree
+git clone https://gitlab.com/improbir/proprietary_vendor_realme_rmx1921.git -b snow vendor/realme/RMX1921 
+
+#Realme Dirac
+git clone https://github.com/improbir/packages_apps_RealmeDirac.git -b lineage-19.0 packages/apps/RealmeDirac
