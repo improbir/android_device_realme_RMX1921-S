49f50bd8272f849470c8d52ed32ce7af0aeb1c37
officialpro <ghosh_probir@yahoo.com>
2022-08-23 01:16:10 +0530

    Vendorsetup: Migrate VT branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/improbir/kernel_realme_sdm710.git -b snow kernel/realme/sdm710 
 
 #Vendor Tree
-git clone https://gitlab.com/improbir/proprietary_vendor_realme_rmx1921.git -b snow vendor/realme/RMX1921 
+git clone https://gitlab.com/improbir/proprietary_vendor_realme_rmx1921.git -b thirteen vendor/realme/RMX1921 
 
 #Realme Dirac
 git clone https://github.com/improbir/packages_apps_RealmeDirac.git -b lineage-19.0 packages/apps/RealmeDirac
