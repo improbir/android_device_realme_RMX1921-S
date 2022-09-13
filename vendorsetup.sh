c47d651df584ce32ad6eeb361b4b2a2db668756d
officialpro <ghosh_probir@yahoo.com>
2022-09-13 15:55:34 +0530

    vendorsetup: Migrate KT branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #Kernel Tree
-git clone https://github.com/improbir/kernel_realme_sdm710.git -b snow kernel/realme/sdm710 
+git clone https://github.com/improbir/kernel_realme_sdm710.git -b thirteen kernel/realme/sdm710 
 
 #Vendor Tree
 git clone https://gitlab.com/improbir/proprietary_vendor_realme_rmx1921.git -b thirteen vendor/realme/RMX1921 
